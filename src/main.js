275f5d902f197225fd57df769c9ff8fd17693552
zyc <1083464029@qq.com>
2019-07-10 22:31:07 +0800

    header finished

--- src/main.js
+++ src/main.js
@@ -4,12 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//引入reset.css
+import './assets/styles/reset.css'
+
+//引入border.css
+import './assets/styles/border.css'
+
+//引入iconfont
+import './assets/styles/iconfont.css'
+
+//解决300ms延迟
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class='header'>
+        <div class='header-left'>
+            <span class='iconfont'>&#xe60e;</span>
+        </div>
+        <div class='header-input'>
+            <span class='iconfont'>&#xe600;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class='header-right'>
+            北京
+            <span class='iconfont'>&#xe6aa;</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+.header
+    width: 100%
+    line-height:.88rem
+    background: #00bcd4
+    color: #fff
+    font-size: .36rem
+    display: flex
+    .header-left
+        width: .4rem
+        padding: 0 .2rem
+        text-align: center
+        float: left
+        font-weight: bold
+    .header-input
+        flex: 1
+        background: #fff
+        height: .6rem
+        margin: .14rem 0
+        border-radius: .1rem
+        color: #e4e7ea
+        line-height: .6rem
+        font-size: .28rem
+        padding-left: .2rem
+        padding-top: .02rem
+    .header-right
+        width: 1.24rem
+        padding: 0 .2rem
+        float: right  
+        text-align: center    
+        font-weight: bold     
+</style>
