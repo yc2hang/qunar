482cf21aa73c613ea8f4ceb58b8107e5575ca216
zyc <1083464029@qq.com>
2019-07-11 23:34:06 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,24 @@
         <home-header></home-header>
         <HomeSwiper></HomeSwiper>
         <HomeIcons></HomeIcons>
+        <HomeRecommend></HomeRecommend>
+        <HomeWeekend></HomeWeekend>
     </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
     name:'Home',
     components:{
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        HomeRecommend,
+        HomeWeekend
     }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <div class='recommend-title'>热销推荐</div>
+        <ul>
+            <li class='item border-bottom' v-for="item in recommendList" :key="item.id">
+                 <img :src="item.imgUrl" alt="" class="item-img">
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeRecommend',
+    data() {
+        return {
+            recommendList:[
+                {
+                    id:'001',
+                    imgUrl:require('@/assets/imgs/recommend001.png'),
+                    title:'浙江出发一日游',
+                    desc:'【去哪儿精选】10-12点晚出发懒北京八达岭长城+往返直通车'
+                },
+                {
+                    id:'002',
+                    imgUrl:require('@/assets/imgs/recommend002.jpg'),
+                    title:'浙江出发一日游',
+                    desc:'【去哪儿精选】VIP航空座椅八达岭+十三陵定陵+鸟巢一日游'
+                },
+                {
+                    id:'003',
+                    imgUrl:require('@/assets/imgs/recommend003.jpg'),
+                    title:'浙江出发一日游',
+                    desc:'【中高考生专享】北京杜莎夫人蜡像馆中高考生票'
+                },
+                {
+                    id:'004',
+                    imgUrl:require('@/assets/imgs/recommend004.jpg'),
+                    title:'浙江出发一日游',
+                    desc:'【中高考生专享】太平洋海底世界考生专享票'
+                },
+                {
+                    id:'005',
+                    imgUrl:require('@/assets/imgs/recommend005.jpg'),
+                    title:'浙江出发一日游',
+                    desc:'【去哪儿精选】八达岭长城+颐和园+清华北大+鸟巢水立方一日游'
+                },
+                {
+                    id:'006',
+                    imgUrl:require('@/assets/imgs/recommend006.jpg'),
+                    title:'浙江出发一日游',
+                    desc:'【活动票】金海湖成人票'
+                },
+            ]
+        }
+    },
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .recommend-title
+        line-height:.8rem
+        background:#eee
+        text-indent:.2rem
+        margin-top:.2rem
+    .item
+        display:flex
+        overflow:hidden
+        height:2.1rem
+        .item-img
+            width:3.0rem
+            height:1.5rem
+            padding:.1rem
+        .item-info
+            flex:1
+            padding:.1rem
+            min-width:0
+            .item-title
+                line-height:.54rem
+                font-size:.32rem
+                ellipsis()
+            .item-desc
+                line-height:.4rem
+                color:#ccc
+                ellipsis()
+            .item-button
+                background:#ff9300
+                padding:0 .1rem
+                border-radius:.06rem
+                margin-top:.16rem
+                color:#fff
+                line-height:.44rem
+
+</style>
